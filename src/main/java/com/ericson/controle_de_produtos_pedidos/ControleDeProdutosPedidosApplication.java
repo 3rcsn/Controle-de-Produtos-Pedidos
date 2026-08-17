4d38a3153ac083f57341f9217cb1488a08538163
ercsn <33rcsn@gmail.com>
2026-08-17 20:13:51 -0300

    Início do projeto
    Setup inicial do projeto utilizando spring initializr.

---
+++ src/main/java/com/ericson/controle_de_produtos_pedidos/ControleDeProdutosPedidosApplication.java
@@ -0,0 +1,13 @@
+package com.ericson.controle_de_produtos_pedidos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControleDeProdutosPedidosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ControleDeProdutosPedidosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ericson/controle_de_produtos_pedidos/ControleDeProdutosPedidosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ericson.controle_de_produtos_pedidos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ControleDeProdutosPedidosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
