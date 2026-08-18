38819959c13483b6fe6c3f40e813f036579402f5
ercsn <33rcsn@gmail.com>
2026-08-18 17:54:12 -0300

    Repository de Departamento.
    Criado repositório de Departamento para tratar consultas e comandos SQL via JPA.

---
+++ src/main/java/com/ericson/controle_de_produtos_pedidos/repository/DepartamentoRepository.java
@@ -0,0 +1,9 @@
+package com.ericson.controle_de_produtos_pedidos.repository;
+
+import com.ericson.controle_de_produtos_pedidos.model.DepartamentoEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface DepartamentoRepository extends JpaRepository<DepartamentoEntity, Integer> {
+}
