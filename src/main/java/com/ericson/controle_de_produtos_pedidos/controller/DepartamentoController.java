3e7705739ed54e61fc998fe5139ea7fbf71360ce
ercsn <33rcsn@gmail.com>
2026-08-18 17:25:27 -0300

    Controller Departamento
    Adiciona controller de Departamento para receber e tratar requisições http.

---
+++ src/main/java/com/ericson/controle_de_produtos_pedidos/controller/DepartamentoController.java
@@ -0,0 +1,9 @@
+package com.ericson.controle_de_produtos_pedidos.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/departamento")
+public class DepartamentoController {
+}
