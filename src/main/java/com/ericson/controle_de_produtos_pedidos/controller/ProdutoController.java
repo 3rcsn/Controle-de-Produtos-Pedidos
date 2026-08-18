10345bf210bed1278fe4554d49e99be500d3990a
ercsn <33rcsn@gmail.com>
2026-08-18 17:31:14 -0300

    Controller de Produto
    Adiciona controller de Produto para receber e tratar requisições http.

---
+++ src/main/java/com/ericson/controle_de_produtos_pedidos/controller/ProdutoController.java
@@ -0,0 +1,21 @@
+package com.ericson.controle_de_produtos_pedidos.controller;
+
+import com.ericson.controle_de_produtos_pedidos.dto.ProdutoDto;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/produto")
+public class ProdutoController {
+
+    /*@PostMapping
+    public ResponseEntity<?> inserir (@RequestBody ProdutoDto produto) {
+
+    }*/
+
+
+
+}
