b86e4ac1422ebdda83d21a0b6836c98778850008
ercsn <33rcsn@gmail.com>
2026-08-20 00:01:57 -0300

    Ajuste dto de prpdutos e pedidos.

--- src/main/java/com/ericson/controle_de_produtos_pedidos/dto/PedidoDto.java
+++ src/main/java/com/ericson/controle_de_produtos_pedidos/dto/PedidoDto.java
@@ -1,7 +1,8 @@
 package com.ericson.controle_de_produtos_pedidos.dto;
 
 import java.util.Date;
+import java.util.List;
 
-public record PedidoDto(Integer numero, Date data, ProdutoDto produto) {
+public record PedidoDto(Integer numero, Date data, List<ProdutoPedidoDto> produto) {
 
 }
