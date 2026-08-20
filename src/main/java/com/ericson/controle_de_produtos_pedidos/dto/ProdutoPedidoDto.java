f9df2abe0921e89cff775d5418db34513ec49686
ercsn <33rcsn@gmail.com>
2026-08-20 00:02:15 -0300

    Ajuste produto pedido dto.

--- src/main/java/com/ericson/controle_de_produtos_pedidos/dto/ProdutoPedidoDto.java
+++ src/main/java/com/ericson/controle_de_produtos_pedidos/dto/ProdutoPedidoDto.java
@@ -1,5 +1,7 @@
 package com.ericson.controle_de_produtos_pedidos.dto;
 
-public record ProdutoPedidoDto (Integer quantidade, Double precoProduto) {
+import java.math.BigDecimal;
+
+public record ProdutoPedidoDto (Integer quantidade, BigDecimal precoProduto) {
 
 }
