7c1d655d49d706bdd43ff21a3da3c54843f2a24d
ercsn <33rcsn@gmail.com>
2026-08-19 23:57:28 -0300

    Produto pedido entity.

---
+++ src/main/java/com/ericson/controle_de_produtos_pedidos/model/ProdutoPedidoEntity.java
@@ -0,0 +1,26 @@
+package com.ericson.controle_de_produtos_pedidos.model;
+
+import jakarta.persistence.*;
+
+import java.math.BigDecimal;
+
+@Entity
+@Table(name = "produto_pedido")
+public class ProdutoPedidoEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private Integer quantidade;
+    @Column(precision = 10, scale = 2)
+    private BigDecimal precoProduto;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "pedido_numero", nullable = false)
+    private PedidoEntity pedido;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "produto_codigo", nullable = false)
+    private ProdutoEntity produto;
+
+}
