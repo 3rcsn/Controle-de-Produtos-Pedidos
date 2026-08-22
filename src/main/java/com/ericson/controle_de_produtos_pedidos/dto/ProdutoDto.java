eb5427612b6832fa1f20b07fb86914b77fcafbf6
ercsn <33rcsn@gmail.com>
2026-08-22 12:46:46 -0300

    Produto Dto

--- src/main/java/com/ericson/controle_de_produtos_pedidos/dto/ProdutoDto.java
+++ src/main/java/com/ericson/controle_de_produtos_pedidos/dto/ProdutoDto.java
@@ -1,7 +1,23 @@
 package com.ericson.controle_de_produtos_pedidos.dto;
 
+import com.ericson.controle_de_produtos_pedidos.model.ProdutoEntity;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
 import java.math.BigDecimal;
 
-public record ProdutoDto (Integer codigo, String descricao, BigDecimal preco) {
+@AllArgsConstructor
+@NoArgsConstructor
+public class ProdutoDto {
+    private Integer codigo;
+    private String descricao;
+    private BigDecimal preco;
 
+    public ProdutoDto paraDto(ProdutoEntity produtoEntity) {
+        this.codigo = produtoEntity.getCodigo();
+        this.descricao = produtoEntity.getDescricao();
+        this.preco = produtoEntity.getPreco();
+
+        return this;
+    }
 }
