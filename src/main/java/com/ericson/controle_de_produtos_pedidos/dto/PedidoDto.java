8f9e35ad1b7068cd71678267188c5482506c92a3
ercsn <33rcsn@gmail.com>
2026-08-22 12:46:36 -0300

    Pedido dto

--- src/main/java/com/ericson/controle_de_produtos_pedidos/dto/PedidoDto.java
+++ src/main/java/com/ericson/controle_de_produtos_pedidos/dto/PedidoDto.java
@@ -1,8 +1,26 @@
 package com.ericson.controle_de_produtos_pedidos.dto;
 
+import com.ericson.controle_de_produtos_pedidos.model.PedidoEntity;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 import java.util.Date;
 import java.util.List;
 
-public record PedidoDto(Integer numero, Date data, List<ProdutoPedidoDto> produto) {
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class PedidoDto {
+    private Integer numero;
+    private Date data;
+    private List<ProdutoPedidoDto> produto;
 
+    public PedidoDto fromEntity(PedidoEntity pedidoEntity) {
+        return new PedidoDto(pedidoEntity.getNumero(), pedidoEntity.getData(), pedidoEntity.getProdutos().stream().map(ProdutoPedidoDto::new).toList());
+    }
 }
+
+
