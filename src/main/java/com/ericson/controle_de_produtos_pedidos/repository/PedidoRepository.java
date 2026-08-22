90386a647348e9ddcd665c3e24116eee2a468bed
ercsn <33rcsn@gmail.com>
2026-08-22 12:50:07 -0300

    Ajuste consulta pedidos por perido.

--- src/main/java/com/ericson/controle_de_produtos_pedidos/repository/PedidoRepository.java
+++ src/main/java/com/ericson/controle_de_produtos_pedidos/repository/PedidoRepository.java
@@ -3,6 +3,7 @@ package com.ericson.controle_de_produtos_pedidos.repository;
 import com.ericson.controle_de_produtos_pedidos.model.PedidoEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.time.LocalDate;
@@ -13,19 +14,22 @@ public interface PedidoRepository extends JpaRepository<PedidoEntity, Integer> {
 
     @Query(value = """
             SELECT
-                p.numero,
-                p.data,
-                pp.codigo_produto,
-                prod.descricao,
-                pp.quantidade,
+                p.numero as numero,
+                p.data as data,
+                p.codigo as codigo,
+                p.descricao as descricao,
+                pp.quantidade as quantidade,
                 (pp.quantidade * pp.preco_produto) as total_item
-            FROM PEDIDO p
-                JOIN PRODUTO_PEDIDO pp ON (p.numero = pp.pedido_numero)
-                JOIN PRODUTO prod ON (pp.codigo_produto = p.codigo)
-            WHERE p.data BETWEEN ?1 and ?2
+            FROM pedido p
+                JOIN produto_pedido pp ON (p.numero = pp.numero)
+                JOIN produto prod ON (pp.codigo = prod.codigo)
+            WHERE p.data BETWEEN :dataInicial and :dataFinal
             ORDER BY
-             p.numero, pp.codigo_produto
+                p.numero, prod.codigo
             """,  nativeQuery = true)
-    List<PedidoEntity> buscarPorPeriodo(LocalDate dataInicial, LocalDate dataFinal);
+    List<PedidoEntity> buscarPorPeriodo(
+            @Param("dataInicial") LocalDate dataInicial,
+            @Param("dataFinal") LocalDate dataFinal
+    );
 
 }
