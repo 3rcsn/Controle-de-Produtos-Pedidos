76d050b2150d29c68c78c8eba27939f05d8c19c0
ercsn <33rcsn@gmail.com>
2026-08-22 12:46:24 -0300

    Departamento dto.

--- src/main/java/com/ericson/controle_de_produtos_pedidos/dto/DepartamentoDto.java
+++ src/main/java/com/ericson/controle_de_produtos_pedidos/dto/DepartamentoDto.java
@@ -1,5 +1,13 @@
 package com.ericson.controle_de_produtos_pedidos.dto;
 
-public record DepartamentoDto (Integer codigo, String descricao) {
+import com.ericson.controle_de_produtos_pedidos.model.DepartamentoEntity;
 
+public class DepartamentoDto {
+    private Integer codigo;
+    private String descricao;
+
+    public DepartamentoDto(DepartamentoEntity departamentoEntity) {
+        this.codigo = departamentoEntity.getCodigo();
+        this.descricao = departamentoEntity.getDescricao();
+    }
 }
