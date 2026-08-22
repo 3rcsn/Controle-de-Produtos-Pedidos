1a36cebb03b0b35658b528358d1a1e24a9dd076b
ercsn <33rcsn@gmail.com>
2026-08-22 12:47:57 -0300

    Relacionamento pedido produtos.

--- src/main/java/com/ericson/controle_de_produtos_pedidos/model/PedidoEntity.java
+++ src/main/java/com/ericson/controle_de_produtos_pedidos/model/PedidoEntity.java
@@ -2,6 +2,10 @@ package com.ericson.controle_de_produtos_pedidos.model;
 
 import com.ericson.controle_de_produtos_pedidos.dto.PedidoDto;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.util.ArrayList;
 import java.util.Date;
@@ -9,19 +13,25 @@ import java.util.List;
 
 @Entity
 @Table(name = "pedido")
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
 public class PedidoEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer numero;
     private Date data;
 
-    @OneToMany(mappedBy = "pedido_numero", cascade = CascadeType.ALL, orphanRemoval = true)
-    private List<ProdutoPedido> produtos = new ArrayList<>();
+    @OneToMany(mappedBy = "pedido", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<ProdutoPedidoEntity> produtos = new ArrayList<>();
 
     public PedidoEntity(PedidoDto pedidoDto) {
-        this.numero = pedidoDto.numero();
-        this.data = pedidoDto.data();
+        this.numero = pedidoDto.getNumero();
+        this.data = pedidoDto.getData();
+        this.produtos = pedidoDto.getProduto().stream().map(ProdutoPedidoEntity::new).toList();
     }
+
 }
 
 
