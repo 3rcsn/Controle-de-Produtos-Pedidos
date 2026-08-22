a7f8e06829769aaa0f805b96c56e8610d91b77dd
ercsn <33rcsn@gmail.com>
2026-08-22 12:47:44 -0300

    Relacionamento dpto produto.

--- src/main/java/com/ericson/controle_de_produtos_pedidos/model/DepartamentoEntity.java
+++ src/main/java/com/ericson/controle_de_produtos_pedidos/model/DepartamentoEntity.java
@@ -1,14 +1,28 @@
 package com.ericson.controle_de_produtos_pedidos.model;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.List;
 
 @Entity
 @Table(name = "departamento")
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
 public class DepartamentoEntity {
 
+    @Setter
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer codigo;
     private String descricao;
+
+    @OneToMany(mappedBy = "departamento", fetch = FetchType.LAZY, orphanRemoval = true)
+    private List<ProdutoEntity> produtos;
 
 }
