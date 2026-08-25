0c4f924528ae63639fcd4a9ac896337c6113b39c
ercsn <33rcsn@gmail.com>
2026-08-24 23:24:39 -0300

    Ajuste entidade ProdutoPedido.

--- src/main/java/com/ericson/controle_de_produtos_pedidos/model/ProdutoPedido.java
+++ src/main/java/com/ericson/controle_de_produtos_pedidos/model/ProdutoPedido.java
@@ -1,35 +1,81 @@
 package com.ericson.controle_de_produtos_pedidos.model;
 
+import com.ericson.controle_de_produtos_pedidos.dto.ProdutoPedidoDto;
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import org.springframework.beans.BeanUtils;
 
 import java.math.BigDecimal;
 
 @Entity
 @Table(name = "produto_pedido")
-@Getter
-@Setter
 public class  ProdutoPedido {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "id")
     private Long id;
 
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "pedido_numero", referencedColumnName = "numero")
+    @ManyToOne
+    @JoinColumn(name = "numero", referencedColumnName = "numero")
     private Pedido pedido;
 
-    @ManyToOne
-    @JoinColumn(name = "produto_codigo", referencedColumnName = "codigo")
-    private Produto produto;
+    private Integer codigo;
 
     private Integer quantidade;
 
     @Column(precision = 10, scale = 2)
-    private BigDecimal preco;
+    private BigDecimal precoUnitario;
 
+    @Transient
+    private BigDecimal precoTotal;
+
+    public ProdutoPedido(ProdutoPedidoDto produtoPedidoDto) {
+        if (produtoPedidoDto != null) {
+            BeanUtils.copyProperties(produtoPedidoDto, this);
+        }
+    }
+
+    public ProdutoPedido() {}
+
+    public ProdutoPedido(Pedido pedido, Integer codigo, Integer quantidade, BigDecimal precoUnitario) {
+        this.pedido = pedido;
+        this.codigo = codigo;
+        this.quantidade = quantidade;
+        this.precoUnitario = precoUnitario;
+    }
+
+    public Pedido getPedido() {
+        return pedido;
+    }
+
+    public void setPedido(Pedido pedido) {
+        this.pedido = pedido;
+    }
+
+    public Integer getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(Integer codigo) {
+        this.codigo = codigo;
+    }
+
+    public BigDecimal getPrecoUnitario() {
+        return precoUnitario;
+    }
+
+    public void setPrecoUnitario(BigDecimal precoUnitario) {
+        this.precoUnitario = precoUnitario;
+    }
+
+    public Integer getQuantidade() {
+        return quantidade;
+    }
+
+    public void setQuantidade(Integer quantidade) {
+        this.quantidade = quantidade;
+    }
+
+    public BigDecimal getPrecoTotal() { return precoTotal; }
+
+    public void setPrecoTotal(BigDecimal precoTotal) { this.precoTotal = precoTotal; }
 }
