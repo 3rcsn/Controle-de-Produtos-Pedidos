f6910f287f0185be17546255b4346bb5858f06b6
ercsn <33rcsn@gmail.com>
2026-08-24 23:18:34 -0300

    Popular dados produto e dpto.

---
+++ src/main/resources/import.sql
@@ -0,0 +1,4 @@
+INSERT INTO departamento (codigo, descricao) VALUES (1, 'Frios');
+INSERT INTO departamento (codigo, descricao) VALUES (2, 'Ferragens');
+INSERT INTO produto (codigo, descricao, preco, departamento_codigo) VALUES (1, 'Tilápia', 10.99, 1);
+INSERT INTO produto (codigo, descricao, preco, departamento_codigo) VALUES (2, 'Parafuso', 5.99, 2);
