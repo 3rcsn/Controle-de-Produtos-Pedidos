e5ceb09bc4f05d7aab1fddba4ee716f696219e04
ercsn <33rcsn@gmail.com>
2026-08-24 23:21:02 -0300

    Utilizar Dto para comunicação.

--- src/main/java/com/ericson/controle_de_produtos_pedidos/service/PedidoService.java
+++ src/main/java/com/ericson/controle_de_produtos_pedidos/service/PedidoService.java
@@ -1,12 +1,16 @@
 package com.ericson.controle_de_produtos_pedidos.service;
 
+import com.ericson.controle_de_produtos_pedidos.dto.PedidoDto;
 import com.ericson.controle_de_produtos_pedidos.model.Pedido;
+import com.ericson.controle_de_produtos_pedidos.model.ProdutoPedido;
 import com.ericson.controle_de_produtos_pedidos.repository.PedidoRepository;
 import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.math.BigDecimal;
 import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -16,8 +20,21 @@ public class PedidoService {
     @Autowired
     PedidoRepository pedidoRepository;
 
-    public Pedido inserirPedido (Pedido pedido) {
-        return pedidoRepository.save(pedido);
+    public PedidoDto inserirPedidoDto (PedidoDto pedidoDto) {
+
+        Pedido pedidoEntidade = new Pedido(pedidoDto);
+
+        List<ProdutoPedido> produtosAux = new ArrayList<>();
+
+        pedidoDto.getProdutos().stream().forEach(produtoPedidoDto -> {
+            ProdutoPedido produtoPedido = new ProdutoPedido(produtoPedidoDto);
+            produtoPedido.setPedido(pedidoEntidade);
+            produtosAux.add(produtoPedido);
+        });
+
+        pedidoEntidade.setProdutos(produtosAux);
+
+        return new PedidoDto(pedidoRepository.save(pedidoEntidade));
     }
 
     public Pedido atualizar(Pedido pedidoAtualizado) {
@@ -30,16 +47,39 @@ public class PedidoService {
         pedidoRepository.deleteById(numero);
     }
 
-    public Pedido consultarPorId(Integer numero) {
-        return pedidoRepository.findById(numero)
-                .orElseThrow(() -> new RuntimeException("Pedido não encontrado"));
+    public PedidoDto consultarPorId(Integer numero) {
+
+        PedidoDto pedido = new PedidoDto(pedidoRepository.findById(numero)
+                .orElseThrow(() -> new RuntimeException("Pedido não encontrado")));
+
+        pedido.setValorTotal(calcularValorTotalPedido(pedido));
+
+        return pedido;
+
     }
 
-    public List<Pedido> getPedidosPorPeriodo(LocalDate dataInicial, LocalDate dataFinal){
+    public List<PedidoDto> getPedidosPorPeriodo(LocalDate dataInicial, LocalDate dataFinal){
 
-        return pedidoRepository.buscarPorPeriodo(dataInicial, dataFinal)
-                .stream().toList();
+        List<PedidoDto> pedidos = pedidoRepository.buscarPorPeriodo(dataInicial, dataFinal)
+                .stream().map(PedidoDto::new).toList();
 
+        pedidos.forEach(pedidoDto -> {
+            pedidoDto.setValorTotal(calcularValorTotalPedido(pedidoDto));
+        });
+
+        return pedidos;
+
+    }
+
+    private BigDecimal calcularValorTotalPedido(PedidoDto pedido) {
+
+        BigDecimal valorTotal = new BigDecimal(0);
+
+        pedido.getProdutos().forEach(produtoPedidoDto -> {
+            valorTotal.add(produtoPedidoDto.getPrecoTotal());
+        });
+
+        return valorTotal;
     }
 
 }
