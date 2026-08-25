0cc843b99b550456535148799ac1bb19134f6e6c
ercsn <33rcsn@gmail.com>
2026-08-24 23:24:08 -0300

    Ajuste entidade Pedido.

--- src/main/java/com/ericson/controle_de_produtos_pedidos/model/Pedido.java
+++ src/main/java/com/ericson/controle_de_produtos_pedidos/model/Pedido.java
@@ -1,10 +1,8 @@
 package com.ericson.controle_de_produtos_pedidos.model;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import org.springframework.beans.BeanUtils;
+import com.ericson.controle_de_produtos_pedidos.dto.PedidoDto;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
@@ -12,29 +10,66 @@ import java.util.List;
 
 @Entity
 @Table(name = "pedido")
-@Getter
-@Setter
-@NoArgsConstructor
-@AllArgsConstructor
 public class Pedido {
     @Id
     @Column(name = "numero")
     private Integer numero;
-    @Column(name = "data", nullable = false)
     private LocalDate data;
 
     @OneToMany(mappedBy = "pedido", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    @PrimaryKeyJoinColumn
     private List<ProdutoPedido> produtos;
 
     @Transient
-    private BigDecimal totalPedido;
+    private BigDecimal valorTotal;
 
-    public Pedido(Integer numero, LocalDate data, List<ProdutoPedido> produto) {
-        this.numero = numero;
-        this.data = data;
-        this.produtos = produto;
+    public Pedido() {
     }
 
+    public Pedido(Integer numero, LocalDate data, List<ProdutoPedido> produtos, BigDecimal valorTotal) {
+        this.numero = numero;
+        this.data = data;
+        this.produtos = produtos;
+        this.valorTotal = valorTotal;
+    }
+
+    public Pedido(PedidoDto pedidoDto) {
+        if (pedidoDto != null && pedidoDto.getProdutos() != null) {
+            BeanUtils.copyProperties(pedidoDto, this);
+        }
+    }
+
+    public Integer getNumero() {
+        return numero;
+    }
+
+    public void setNumero(Integer numero) {
+        this.numero = numero;
+    }
+
+    public LocalDate getData() {
+        return data;
+    }
+
+    public void setData(LocalDate data) {
+        this.data = data;
+    }
+
+    public List<ProdutoPedido> getProdutos() {
+        return produtos;
+    }
+
+    public void setProdutos(List<ProdutoPedido> produtos) {
+        this.produtos = produtos;
+    }
+
+    public BigDecimal getTotalPedido() {
+        return valorTotal;
+    }
+
+    public void setTotalPedido(BigDecimal valorTotal) {
+        this.valorTotal = valorTotal;
+    }
 }
 
 
